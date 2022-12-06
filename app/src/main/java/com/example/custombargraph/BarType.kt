f85d1be34dbdce3dbc83f34dba5933f7708fa36b
Aditya Tech Developers <adityatechdevelopers@gmail.com>
2022-12-06 06:10:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/custombargraph/BarType.kt
@@ -0,0 +1,8 @@
+package com.example.custombargraph
+
+enum class BarType {
+
+    CIRCULAR_TYPE,
+    TOP_CURVED
+
+}
---
+++ app/src/main/java/com/example/custombargraph/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.custombargraph
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.custombargraph.ui.theme.CustomBarGraphTheme
+import com.example.custombargraph.ui.theme.Purple500
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomBarGraphTheme {
+                // A surface container using the 'background' color from the theme
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 30.dp)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    val dataList = mutableListOf(30,60,90,50,70)
+                    val floatValue = mutableListOf<Float>()
+                    val datesList = mutableListOf(2,3,4,5,6)
+
+                    dataList.forEachIndexed { index, value ->
+
+                        floatValue.add(index = index, element = value.toFloat()/dataList.max().toFloat())
+
+                    }
+
+                    BarGraph(
+                        graphBarData = floatValue,
+                        xAxisScaleData = datesList,
+                        barData_ = dataList,
+                        height = 300.dp,
+                        roundType = BarType.CIRCULAR_TYPE,
+                        barWidth = 20.dp,
+                        barColor = Purple500,
+                        barArrangement = Arrangement.SpaceEvenly
+                    )
+                }
+            }
+        }
+    }
+}
